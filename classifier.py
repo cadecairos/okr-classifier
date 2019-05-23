1e8b46826e36d6a53d934b3dc6638b95de1b773e
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-23 13:07:08 -0400

    initial commit

---
+++ classifier.py
@@ -0,0 +1,66 @@
+# Tensorflow and Keras
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout
+
+# Helper libraries
+from sklearn.preprocessing import LabelBinarizer
+import numpy as np
+import pandas as pd
+import csv
+
+
+# read the dataset in to data frames
+objective_df = pd.read_csv('./data/objectives.csv', header=0)
+
+# 80% of data for training, 20% for testing
+train_size = int(len(objective_df) * .8)
+train_objectives = objective_df['Objective'].values[:train_size]
+train_labels = objective_df['Label'].values[train_size]
+
+test_objectives = objective_df['Objective'].values[train_size:]
+test_labels = objective_df['Label'].values[train_size:]
+
+# 0=good, 1=bad
+classes = ['good', 'bad']
+vocab_size = 500
+num_labels = 2
+
+# set a maximum number of words to tokenize
+tokenizer = Tokenizer(num_words=500)
+tokenizer.fit_on_texts(train_objectives)
+
+# Not 100% sure what I'm doing beyond here yet
+x_train = tokenizer.texts_to_matrix(train_objectives, mode='tfidf')
+x_test = tokenizer.texts_to_matrix(test_objectives, mode='tfidf')
+
+encoder = LabelBinarizer()
+encoder.fit(train_labels)
+y_train = encoder.transform(train_labels)
+y_test = encoder.transform(test_labels)
+
+model = Sequential()
+model.add(Dense(56, input_shape=(vocab_size,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.3))
+model.add(Dense(56))
+model.add(Activation('relu'))
+model.add(Dropout(0.3))
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=10,
+                    epochs=100,
+                    verbose=1,
+                    validation_split=0.1)
+
+
+

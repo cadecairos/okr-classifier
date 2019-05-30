9ae77f844855d75638d5e29c9167b6aa167fa7da
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-30 17:28:42 -0400

    saving and loading

--- load_model.py
+++ load_model.py
@@ -1,15 +1,19 @@
 # Tensorflow and Keras
-import tensorflow as tf
-from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
-from keras.models import Sequential
-from keras.layers import Activation, Dense, Dropout
+from keras.models import load_model
 
 # Helper libraries
 from sklearn.preprocessing import LabelBinarizer
-import numpy as np
 import pandas as pd
+import numpy as np
 import pickle
+
+model = load_model('objectives.h5')
+
+tokenizer = Tokenizer()
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
 
 # read the dataset in to data frames
 objective_df = pd.read_csv('./data/objectives.csv', header=0)
@@ -27,8 +31,6 @@ classes = ['good', 'bad']
 vocab_size = 500
 num_labels = 2
 
-# set a maximum number of words to tokenize
-tokenizer = Tokenizer(num_words=500)
 tokenizer.fit_on_texts(train_objectives)
 
 # Not 100% sure what I'm doing beyond here yet
@@ -40,30 +42,10 @@ encoder.fit(train_labels)
 y_train = encoder.transform(train_labels)
 y_test = encoder.transform(test_labels)
 
-model = Sequential()
-model.add(Dense(56, input_shape=(vocab_size,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.3))
-model.add(Dense(56))
-model.add(Activation('relu'))
-model.add(Dropout(0.3))
-model.add(Dense(num_labels))
-model.add(Activation('softmax'))
-
-# output summary of the Model's layers
 model.summary()
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-history = model.fit(x_train, y_train,
-                    batch_size=10,
-                    epochs=8,
-                    verbose=1,
-                    validation_split=0.1)
-
 score = model.evaluate(x_test, y_test, batch_size=10, verbose=1)
+
 
 print(f'Test Accuracy: {score[1]}')
 
@@ -74,9 +56,3 @@ for i in range(10):
     print(f'Actual label: {classes[test_labels[i]]}')
     print(f'Predicted label: {predicted_label}')
     print(f' ------------------ ')
-
-print('saving model...')
-
-model.save('objectives.h5')
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)

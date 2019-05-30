cd82f27f278dfaf7b997eabed5c59cd4c4cecf96
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-30 17:07:07 -0400

    \o/

--- classifier.py
+++ classifier.py
@@ -18,7 +18,7 @@ objective_df = pd.read_csv('./data/objectives.csv', header=0)
 # 80% of data for training, 20% for testing
 train_size = int(len(objective_df) * .8)
 train_objectives = objective_df['Objective'].values[:train_size]
-train_labels = objective_df['Label'].values[train_size]
+train_labels = objective_df['Label'].values[:train_size]
 
 test_objectives = objective_df['Objective'].values[train_size:]
 test_labels = objective_df['Label'].values[train_size:]
@@ -50,17 +50,28 @@ model.add(Activation('relu'))
 model.add(Dropout(0.3))
 model.add(Dense(num_labels))
 model.add(Activation('softmax'))
+
+# output summary of the Model's layers
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
                     batch_size=10,
-                    epochs=100,
+                    epochs=8,
                     verbose=1,
                     validation_split=0.1)
 
+score = model.evaluate(x_test, y_test, batch_size=10, verbose=1)
 
+print(f'Test Accuracy: {score[1]}')
 
+for i in range(10):
+    prediction = model.predict(np.array([x_test[i]]))
+    predicted_label = classes[np.argmax(prediction[0])]
+    print(f'Objective text: {test_objectives[i]}')
+    print(f'Actual label: {classes[test_labels[i]]}')
+    print(f'Predicted label: {predicted_label}')
+    print(f' ------------------ ')
